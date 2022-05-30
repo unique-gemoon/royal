6622b1c27c8d955870e9ed9556bd85df7cbd3f8c
unique-gemoon <christian.renhard0623@gmail.com>
2022-05-30 12:22:59 +0100

    model comment add attribute intendedUserId (utilisateur destiné)

--- app/nodejs/app/models/comment.model.js
+++ app/nodejs/app/models/comment.model.js
@@ -22,6 +22,15 @@ export default function comment(sequelize, Sequelize) {
       allowNull: false,
       onDelete: "cascade",
     },
+    intendedUserId: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: "users",
+        key: "id",
+      },
+      allowNull: false,
+      onDelete: "cascade",
+    },
     parentId: {
       type: Sequelize.INTEGER,
       references: {
@@ -36,6 +45,7 @@ export default function comment(sequelize, Sequelize) {
   Comment.associate = function (models) {
     Comment.belongsTo(models.pli, { foreignKey: "pliId" });
     Comment.belongsTo(models.user, { foreignKey: "userId" });
+    Comment.belongsTo(models.user, { foreignKey: "intendedUserId" });
     Comment.belongsTo(models.comment, { foreignKey: "parentId" });
     Comment.hasMany(models.commentNotifications); 
   };
